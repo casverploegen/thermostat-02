9cbe7703c16d6053a63f3766d081571893663517
s153820 <boumanjaap@gmail.com>
2017-10-23 16:15:19 +0200

    vacation: slider update + buttons

--- js/vacation.js
+++ js/vacation.js
@@ -1,5 +1,19 @@
 var output = document.getElementById("temperature");
+var slider = document.getElementById("slider");
 
-document.getElementById("slider").oninput = function() {
+slider.oninput = function() {
        output.innerHTML = this.value;
    }
+
+document.getElementById("raiseTemp").onclick = function() {
+  var tempChange = Math.round(10 * (parseFloat(output.innerHTML) + 0.1))/10;
+  output.innerHTML = tempChange;
+  slider.value = tempChange;
+}
+
+document.getElementById("lowerTemp").onclick = function() {
+  var tempChange = Math.round(10 * (parseFloat(output.innerHTML) - 0.1))/10;
+  output.innerHTML = tempChange;
+  slider.value = tempChange;
+
+}
